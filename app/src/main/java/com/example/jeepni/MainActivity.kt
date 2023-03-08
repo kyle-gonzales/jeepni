9d78001230f79cf3ac56dd8015e97a5f224360f1
Angel Estano <asestano@up.edu.ph>
2023-03-08 11:28:12 +0800

    textfield 1.1

--- app/src/main/java/com/example/jeepni/MainActivity.kt
+++ app/src/main/java/com/example/jeepni/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -15,6 +16,7 @@ import androidx.compose.ui.*
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.res.*
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jeepni.ui.theme.*
@@ -292,6 +294,27 @@ fun TermsAndConditions(){
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Textfield(){
+    Column(modifier = Modifier.padding(16.dp)) {
+        var name by remember { mutableStateOf("") }
+        Text(
+            text = "Hello! $name",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        TextField(
+            value = name,
+            label = { Text(text = "Phone Number") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = {
+                name = it
+            }
+        )
+    }
+}
+
 /*
 @Preview(showSystemUi = true)
 @Composable
