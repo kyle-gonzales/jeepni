3b61987e825f0194e63e4bd8d9f13d2319489212
Ericka Monique Miranda <emiranda@up.edu.ph>
2023-03-09 14:44:43 +0800

    - added intents in welcome2

--- app/src/main/java/com/example/jeepni/WelcomeActivityLayout2.kt
+++ app/src/main/java/com/example/jeepni/WelcomeActivityLayout2.kt
@@ -1,5 +1,7 @@
 package com.example.jeepni
 
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,16 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.jeepni.ui.theme.Black
 import com.example.jeepni.ui.theme.White
 
 @Composable
 fun WelcomeActivityLayout2(){
-}
+    val context = LocalContext.current
 
-@Composable
-fun LoginOrSignup(){
     Container(0.6f){
         Logo()
         Text(
@@ -26,10 +28,22 @@ fun LoginOrSignup(){
 
         )
         Column{
-            SolidButton() {
+            SolidButton(
+                onClick = {
+                    /*TODO: Go to Sign Up Activity*/
+                    context.startActivity(Intent(context, SignUpActivity::class.java))
+                }
+            ) {
                 Text(stringResource(R.string.sign_up))
             }
-            SolidButton(Black, White) {
+            SolidButton(
+                bgColor = Black,
+                contentColor = White,
+                onClick = {
+                    /*TODO: Go to Log In Activity*/
+                    context.startActivity(Intent(context, LogInActivity::class.java))
+                }
+            ) {
                 Text(stringResource(R.string.log_in))
             }
         }
