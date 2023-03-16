07e263f0fd33aae3d2adc62635120b1b0e414055
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-03-17 01:18:32 +0800

    fixed mistake from merge conflict resolution
    accidentally accepted MainActivityLayout() instead of current change. I also deleted unnecessary imports

--- app/src/main/java/com/example/jeepni/MainActivity.kt
+++ app/src/main/java/com/example/jeepni/MainActivity.kt
@@ -1,36 +1,24 @@
 package com.example.jeepni
 
-import android.content.Intent
 import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.example.jeepni.core.data.repository.AuthRepository
-import com.example.jeepni.feature.authentication.SignUpViewModel
-import com.example.jeepni.feature.home.MainViewModel
 import com.example.jeepni.util.Navigation
-import com.example.jeepni.util.Screen
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.google.rpc.context.AttributeContext.Auth
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 @OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
-//    private val signUpViewModel by viewModels<SignUpViewModel>()
+    //    private val signUpViewModel by viewModels<SignUpViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityLayout()
+            navController = rememberAnimatedNavController()
+            Navigation(navController)
         }
     }
 }
