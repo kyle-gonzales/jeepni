aeaf91c7f91b51c60da7caf908df2d1694d7b092
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-03-19 14:38:34 +0800

    Retrieve user's current analytics from Firestore

--- app/src/main/java/com/example/jeepni/core/data/repository/DailyAnalyticsRepositoryImpl.kt
+++ app/src/main/java/com/example/jeepni/core/data/repository/DailyAnalyticsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.jeepni.core.data.repository
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import com.example.jeepni.core.data.model.DailyAnalytics
 import com.example.jeepni.getCurrentDateString
@@ -40,7 +41,21 @@ class DailyAnalyticsRepositoryImpl (private val auth: FirebaseAuth, private val 
     }
 
     override fun getDailyStats(): Flow<List<DailyAnalytics>>? {
-        // TODO: implement later. remove nullable operator from return type and on interface
+        // NOTE: maybe use a persistent listener instead of a one-time get? https://firebase.google.com/docs/database/android/read-and-write#read_data_with_persistent_listeners
+        usersRef.document(auth.currentUser!!.uid)
+            .collection("analytics")
+            .get()
+            .addOnSuccessListener { result ->
+                // TODO: Turn this into a Flow<List<DailyAnalytics>> somehow
+                for (document in result) {
+                    Log.d("Firebase", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener {
+                Log.i("Firebase", "Error retrieving analytics")
+            }
+
+        // TODO: Remove nullable operator from return type and on interface
         return null
     }
 
