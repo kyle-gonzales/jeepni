61a3f3da193fd855763adb7f0f514a7f42a8bad8
riaqliu <riaqliu@gmail.com>
2023-03-22 13:27:19 +0800

    created a UserCredentials model

---
+++ app/src/main/java/com/example/jeepni/core/data/model/UserCredentials.kt
@@ -0,0 +1,11 @@
+package com.example.jeepni.core.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserCredentials(
+    val email : String,
+    val password : String,
+    val phoneNumber : String,
+): Parcelable
