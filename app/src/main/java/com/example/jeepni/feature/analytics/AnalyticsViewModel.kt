9ed982be670f15603256ed4fa7dd60ef8d34eea7
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-03-22 16:17:10 +0800

    added basic structure of AnalyticsViewModel

--- app/src/main/java/com/example/jeepni/feature/analytics/AnalyticsViewModel.kt
+++ app/src/main/java/com/example/jeepni/feature/analytics/AnalyticsViewModel.kt
@@ -6,9 +6,12 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jeepni.core.data.repository.AuthRepository
 import com.example.jeepni.core.data.repository.DailyAnalyticsRepository
+import com.example.jeepni.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,6 +23,22 @@ class AnalyticsViewModel @Inject constructor(
 
     var analytics = repository.getDailyStats()
 
+    private var _uiEvent = Channel<UiEvent>()
+    var uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event : AnalyticsEvent) {
+        when (event) {
+
+            else -> {}
+        }
+    }
+    private fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
+
+    // used to test the functionality of repository.getDailyStats()
     fun logAnalytics() { // used to test flow
         viewModelScope.launch {
         analytics.collect {analytics ->
@@ -27,5 +46,4 @@ class AnalyticsViewModel @Inject constructor(
             }
         }
     }
-
 }
