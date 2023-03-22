a2e7dae16cb148937b2c099755cafb4eb816df10
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-03-22 20:09:42 +0800

    converted UserDetails model properties to nullable types

--- app/src/main/java/com/example/jeepni/core/data/model/UserDetails.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/UserDetails.kt
@@ -5,6 +5,6 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserDetails(
-    val name : String,
-    val route : String,
+    val name : String?,
+    val route : String?,
 ): Parcelable
