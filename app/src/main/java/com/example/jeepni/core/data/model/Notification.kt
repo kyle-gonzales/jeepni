58f48992f8a37b743772e394ea035694709c8693
riaqliu <riaqliu@gmail.com>
2023-04-26 19:47:34 +0800

    Added notification model, not sure if this is the correct way to implement it

--- app/src/main/java/com/example/jeepni/core/data/model/Notification.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/Notification.kt
@@ -3,9 +3,9 @@ package com.example.jeepni.core.data.model
 import android.os.Parcelable
 import com.example.jeepni.feature.home.getCurrentDateString
 import kotlinx.parcelize.Parcelize
-
 @Parcelize
 data class Notification(
-    val date: String = getCurrentDateString(),
+    val notifyDate: String = getCurrentDateString(),
     val type: String = "default",
+    val userID: String = "0",
 ) : Parcelable
