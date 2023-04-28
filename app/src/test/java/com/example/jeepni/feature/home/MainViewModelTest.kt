2f90fc4a92b3350c47defcaad8d80d1dcc24b00a
riaqliu <riaqliu@gmail.com>
2023-04-28 22:39:25 +0800

    Expanded on the permissions unit test

--- app/src/test/java/com/example/jeepni/feature/home/MainViewModelTest.kt
+++ app/src/test/java/com/example/jeepni/feature/home/MainViewModelTest.kt
@@ -36,12 +36,14 @@ class MainViewModelTest {
     fun `test if analytics values are correct`() { // Testing Mockito stuff
         val badSalary = "0.0.0.0"
         val badFuelCost = "0.0.0.0"
-        viewModel.onEvent((MainEvent.OnSalaryChange(badSalary)))
+
+        viewModel.onEvent((MainEvent.OnSalaryChange(badSalary))) // See if salary data is equal to badSalary
         Assert.assertTrue(viewModel.salary == badSalary)
-        viewModel.onEvent((MainEvent.OnFuelCostChange(badFuelCost)))
+
+        viewModel.onEvent((MainEvent.OnFuelCostChange(badFuelCost)))  // See if fuelCost data is equal to fuelCost
         Assert.assertTrue(viewModel.fuelCost == badFuelCost)
 
-        // Both should return False
+        // Both should return False since badSalary and badFuelCost are not valid inputs
         Assert.assertFalse(viewModel.isValidSalary)
         Assert.assertFalse(viewModel.isValidFuelCost)
     }
@@ -53,16 +55,18 @@ class MainViewModelTest {
         val lat = loc.latitude
         val lon = loc.longitude
 
-        Assert.assertTrue(lat == 10.3157) // Latitude and Longitude Values in MainViewModel.kt
-        Assert.assertTrue(lon == 123.8854)
+        Assert.assertTrue(lat == 10.3157)   // Latitude and Longitude Values in MainViewModel.kt
+        Assert.assertTrue(lon == 123.8854)  // Simply check if the camera is at the specific longitude and latitude
     }
 
     @Test
     fun `test if location on camera position has changed`() {
-        val fakeLat = 13.1666
+        val fakeLat = 13.1666       // faked values can be changed to whatever value
         val fakeLon = 172.5030
         val targetPosition = LatLng(fakeLat, fakeLon)
 
+        // NOTE: had to change viewModel.cameraPositionState into a public property to access it,
+        // might change it once i understand how to use spyk or ReflectionUtils
         viewModel.cameraPositionState =
             CameraPositionState(CameraPosition.fromLatLngZoom(targetPosition, 15f))
 
@@ -70,22 +74,47 @@ class MainViewModelTest {
         val lat = loc.latitude
         val lon = loc.longitude
 
+        // simply check if faked values are reflected in viewModel.cameraPositionState
         Assert.assertTrue(lat == fakeLat)
         Assert.assertTrue(lon == fakeLon)
     }
 
     @Test
     fun `test if permissions are given properly`() {
-        val perms = listOf(
-            "ACCESS_COARSE_LOCATION",
-            "ACCESS_FINE_LOCATION"
+        Assert.assertArrayEquals(arrayOf(), viewModel.visiblePermissionDialogQueue.toTypedArray())
+
+        val permA = "ACCESS_COARSE_LOCATION"
+        val permB = "ACCESS_FINE_LOCATION"
+
+        viewModel.onPermissionResult(permA, false) // This should add the perm into the array
+        Assert.assertArrayEquals(
+            arrayOf(permA),
+            viewModel.visiblePermissionDialogQueue.toTypedArray()
+        )
+        viewModel.onPermissionResult(
+            permA,
+            false
+        ) // This should not add the perm into the array since the perm already exists
+        Assert.assertArrayEquals(
+            arrayOf(permA),
+            viewModel.visiblePermissionDialogQueue.toTypedArray()
         )
 
-        for (perm in perms) {
-            viewModel.onPermissionResult(perm, false)
-            Assert.assertTrue(perm in viewModel.visiblePermissionDialogQueue)
-        }
+        viewModel.onPermissionResult(
+            permB,
+            true
+        ) // Perm B should not be added into the array since request was approved
+        Assert.assertArrayEquals(
+            arrayOf(permA),
+            viewModel.visiblePermissionDialogQueue.toTypedArray()
+        )
 
+        viewModel.dismissDialog()
+        Assert.assertArrayEquals(
+            arrayOf(),
+            viewModel.visiblePermissionDialogQueue.toTypedArray()
+        ) // array should be empty
     }
+
 
 }
