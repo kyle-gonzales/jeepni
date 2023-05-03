b17cb6af2f576e8d109fb6bbe860db4170bacfcb
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-03 17:48:58 +0800

    created broadcast receiver class to listen for incoming alarms

---
+++ app/src/main/java/com/example/jeepni/util/AlarmBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.jeepni.util
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+abstract class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
