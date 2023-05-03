7aa725331459c1c0b5001f834e9e67a5ab7060a5
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-03 22:11:13 +0800

    created basic structure for alarm broadcast receiver
    this broadcast receiver will allow jeepni to observe for any alarm broadcasts, and subsequently send a notification even when the app is not open

--- app/src/main/java/com/example/jeepni/util/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/jeepni/util/AlarmBroadcastReceiver.kt
@@ -3,10 +3,21 @@ package com.example.jeepni.util
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 abstract class AlarmBroadcastReceiver : BroadcastReceiver() {
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onReceive(context: Context?, intent: Intent?) {
+        val notification = intent?.getParcelableExtra("notification_content", String::class.java/*holder class*/) ?: return
 
+        showSimpleNotification(
+            context!!,
+            channelId = Constants.CHANNEL_ID,
+            notificationId = 0,
+            textTitle = "",
+            textContent = ""
+        )
     }
 }
