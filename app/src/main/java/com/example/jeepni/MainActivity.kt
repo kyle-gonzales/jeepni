7a1afa2d58bc880057e3b1a82370bc6ab89ba187
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-03 21:52:52 +0800

    created notification channel

--- app/src/main/java/com/example/jeepni/MainActivity.kt
+++ app/src/main/java/com/example/jeepni/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.jeepni
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +15,7 @@ import com.example.jeepni.core.data.repository.AuthRepository
 import com.example.jeepni.core.data.repository.UserDetailRepository
 import com.example.jeepni.core.ui.theme.JeepNiTheme
 import com.example.jeepni.feature.analytics.AnalyticsViewModel
+import com.example.jeepni.util.Constants
 import com.example.jeepni.util.Navigation
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.android.gms.location.*
@@ -48,6 +54,7 @@ class MainActivity : ComponentActivity() {
                     Manifest.permission.POST_NOTIFICATIONS,
                 )
             )
+            createNotificationChannel()
         } else {
             //TODO: support older android versions
         }
@@ -57,5 +64,11 @@ class MainActivity : ComponentActivity() {
                 Navigation(navController, auth, userDetailsRepository)
             }
         }
+    }
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+        }
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
     }
 }
