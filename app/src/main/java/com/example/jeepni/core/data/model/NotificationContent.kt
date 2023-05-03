a87750a319d195d57ba41f70bcfe22fed9ebd2e4
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-04 00:21:29 +0800

    modified NotificationContent properties
    creates a parcelable object which can be passed to intents for notifications

--- app/src/main/java/com/example/jeepni/core/data/model/NotificationContent.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/NotificationContent.kt
@@ -1,13 +1,16 @@
 package com.example.jeepni.core.data.model
 
+import android.os.Parcelable
 import com.example.jeepni.R
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class NotificationContent(
-    private val title : String,
-    private val content : String = "",
-    private val notificationId : Int,
-    private val iconId: Int = R.drawable.samplelogo
-) {
+    val title : String,
+    val content : String = "",
+    val notificationId : Int,
+    val iconId: Int = R.drawable.samplelogo
+) : Parcelable {
     //TODO: finalize this code
 //    fun setIcon() {
 //        var iconIdMap: Map<String, Int> = mapOf(
