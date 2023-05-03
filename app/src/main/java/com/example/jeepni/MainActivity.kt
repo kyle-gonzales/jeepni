83d5aadfcaf46dcadac65963c4ea49c8d5ab23ed
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-03 21:49:20 +0800

    requested for notification and alarm permissions in main activity
    the requests are launched when the app is launched

--- app/src/main/java/com/example/jeepni/MainActivity.kt
+++ app/src/main/java/com/example/jeepni/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jeepni
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
 import com.example.jeepni.core.data.repository.AuthRepository
@@ -18,6 +19,14 @@ import javax.inject.Inject
 @OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val multiplePermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        permissions ->
+        permissions.entries.forEach {
+            val permission = it.key
+            val isGranted = it.value
+        }
+    }
     private lateinit var navController: NavHostController
     @Inject
     lateinit var auth : AuthRepository // automatically injected. no need for initialization
@@ -31,6 +40,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        analyticsViewModel = ViewModelProvider(this)[AnalyticsViewModel::class.java]
 //        analyticsViewModel.logAnalytics()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            multiplePermissionsLauncher.launch(
+                arrayOf(
+                    Manifest.permission.SCHEDULE_EXACT_ALARM,
+                    Manifest.permission.USE_EXACT_ALARM,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                )
+            )
+        } else {
+            //TODO: support older android versions
+        }
         setContent {
             JeepNiTheme {
                 navController = rememberAnimatedNavController()
