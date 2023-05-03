755b660991bd8ada3f13861cdea28091afc15ce6
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-04 00:19:38 +0800

    modified AlarmContent properties
    properties now fit the alarm scheduler better
    
    made the properties public (lol wtf was i thinking; why were they private)

--- app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
@@ -3,7 +3,6 @@ package com.example.jeepni.core.data.model
 import java.time.LocalDateTime
 
 data class AlarmContent(
-    private val isAlarmEnabled: Boolean = false,
-    private val nextAlarmDate: LocalDateTime = LocalDateTime.now().plusMonths(3),
-    private val periodOfAlarm: Pair<Int, String> = Pair(3, "months")
+    val nextAlarmDate: LocalDateTime = LocalDateTime.now().plusMonths(3),
+    val intervalInDays : Int = 90 // repeat every 3 months
 )
