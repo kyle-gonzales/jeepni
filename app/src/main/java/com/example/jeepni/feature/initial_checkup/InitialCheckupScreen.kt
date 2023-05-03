46af423d9f6d556bf5c10cbe03e8fbe98d0cd07f
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-03 23:29:15 +0800

    minor code formatting

--- app/src/main/java/com/example/jeepni/feature/initial_checkup/InitialCheckupScreen.kt
+++ app/src/main/java/com/example/jeepni/feature/initial_checkup/InitialCheckupScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -40,18 +38,15 @@ fun InitialCheckupScreen(
                 UiEvent.PopBackStack -> {
                     onPopBackStack()
                 }
-
                 is UiEvent.Navigate -> {
                     onNavigate(event)
                 }
-
                 is UiEvent.ShowSnackBar -> {
-
                 }
-
                 is UiEvent.ShowToast -> {
                     Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
                 }
+                else -> {}
             }
         }
     }
