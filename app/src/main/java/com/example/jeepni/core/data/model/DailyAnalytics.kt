c8eb7ca2f363d69be8d5b3a0454a1f4b6304d175
riaqliu <riaqliu@gmail.com>
2023-05-04 00:29:56 +0800

    Added the `timer` field on the DailyAnalytics class.

--- app/src/main/java/com/example/jeepni/core/data/model/DailyAnalytics.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/DailyAnalytics.kt
@@ -2,11 +2,13 @@ package com.example.jeepni.core.data.model
 
 import android.os.Parcelable
 import com.example.jeepni.feature.home.getCurrentDateString
+import com.example.jeepni.feature.home.getCurrentTimeString
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class DailyAnalytics (
-    val date : String = getCurrentDateString(),
-    val salary : Double = 0.0,
-    val fuelCost : Double = 0.0,
+data class DailyAnalytics(
+    val date: String = getCurrentDateString(),
+    val timer: String = getCurrentTimeString(),
+    val salary: Double = 0.0,
+    val fuelCost: Double = 0.0,
 ) : Parcelable
