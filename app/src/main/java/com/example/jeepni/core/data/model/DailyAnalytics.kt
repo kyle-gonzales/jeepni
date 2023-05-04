d932e5ec634fce2d01bc8852a8ff8a84134c604e
riaqliu <riaqliu@gmail.com>
2023-05-04 14:54:34 +0800

    changed DailyAnalytics default from `getCurrentTimeString()` to `0.0`

--- app/src/main/java/com/example/jeepni/core/data/model/DailyAnalytics.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/DailyAnalytics.kt
@@ -2,13 +2,12 @@ package com.example.jeepni.core.data.model
 
 import android.os.Parcelable
 import com.example.jeepni.feature.home.getCurrentDateString
-import com.example.jeepni.feature.home.getCurrentTimeString
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DailyAnalytics(
     val date: String = getCurrentDateString(),
-    val timer: String = getCurrentTimeString(),
+    val timer: Double = 0.0,
     val salary: Double = 0.0,
     val fuelCost: Double = 0.0,
 ) : Parcelable
