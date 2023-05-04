b19bcf979b0551a682eaf6e8dfcf71d1b2712c24
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-04 18:52:13 +0800

    Add initial dummy class for UI tests

---
+++ app/src/test/java/com/example/jeepni/feature/home/MyComposeTest.kt
@@ -0,0 +1,10 @@
+package com.example.jeepni.feature.home
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class MyComposeTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
