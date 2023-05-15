f14ba81a27baf0a8ae29699a651d318a59b689ce
riaqliu <riaqliu@gmail.com>
2023-05-15 21:34:59 +0800

    Test Tags for UI testing

---
+++ app/src/main/java/com/example/jeepni/util/TestTags/TestTags.kt
@@ -0,0 +1,9 @@
+package com.example.jeepni.util.TestTags
+
+object TestTags {
+
+    const val DELETE_DAILY_STATS = "DELETE_DAILY_STATS"
+    const val LOG_DAILY_ANALYTICS = "LOG_DAILY_ANALYTICS"
+    const val DRIVING_MODE = "DRIVING_MODE"
+    const val MAIN_DRAWER = "MAIN_DRAWER"
+}
