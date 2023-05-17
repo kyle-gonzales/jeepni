b4e5acbebfb49c551b8a487ab8321054b9299a4b
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-17 13:24:21 +0800

    Remove unused dependency in MapsRepositoryImpl

--- app/src/main/java/com/example/jeepni/core/data/repository/MapsRepositoryImpl.kt
+++ app/src/main/java/com/example/jeepni/core/data/repository/MapsRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.example.jeepni.core.data.repository
 
-import android.app.Application
-import android.content.Context
 import com.example.jeepni.core.data.model.MapPin
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -12,15 +10,8 @@ import kotlinx.coroutines.flow.callbackFlow
 class MapsRepositoryImpl(
     private val auth: FirebaseAuth,
     private val db: FirebaseFirestore,
-    appContext: Application
 ) :
     MapsRepository {
-    private var context: Context
-
-    init {
-        context = appContext.applicationContext
-    }
-
     override suspend fun createPin(pin: MapPin) {
         db.collection("mapPins")
             .document(auth.currentUser!!.uid)
