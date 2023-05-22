233c5a93d70c90ed3f7822e15435c3f079482409
Angel Estano <asestano@up.edu.ph>
2023-05-23 01:37:24 +0800

    add button, color changes

--- app/src/main/java/com/example/jeepni/feature/checkup/CheckUpScreen.kt
+++ app/src/main/java/com/example/jeepni/feature/checkup/CheckUpScreen.kt
@@ -1,22 +1,28 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.example.jeepni.feature.checkup
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.jeepni.R
 import com.example.jeepni.core.ui.BackIconButton
@@ -65,35 +71,61 @@ fun CheckUpScreen (
                     )
                 },
                 content = { paddingValues ->
-                    LazyVerticalGrid(
-                                columns = GridCells.Fixed(2),
-                                contentPadding = paddingValues,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(10.dp)
-                    ) {
-                        //will change this
-                                item {
-                                    Box(modifier = Modifier.padding(8.dp)){
-                                        ComponentCard(
-                                            component = "LTFRB",
-                                            date = "mm/dd/yyyy",
-                                            alarm = "3 months",
-                                            icon = painterResource(R.drawable.tire_repair)
-                                        )
-                                    }
-                                }
-                                item {
-                                    Box(modifier = Modifier.padding(8.dp)){
-                                        ComponentCard(
-                                            component = "Oil Change",
-                                            date = "mm/dd/yyyy",
-                                            alarm = "3 months",
-                                            icon = painterResource(R.drawable.tire_repair)
-                                        )
+                    Box(
+                        modifier = Modifier.background(color = MaterialTheme.colorScheme.inverseOnSurface)
+                    ){
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            contentPadding = paddingValues,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(10.dp)
+                        ) {
+                            item {
+                                Box(modifier = Modifier.padding(8.dp)){
+                                    Row(
+                                        modifier = Modifier
+                                            .border(
+                                                border = BorderStroke(2.dp, color = MaterialTheme.colorScheme.outlineVariant),
+                                                shape = RoundedCornerShape(10.dp)
+                                            )
+                                            .clip(RoundedCornerShape(15.dp))
+                                            .background(MaterialTheme.colorScheme.outlineVariant)
+                                            .requiredWidth(164.5.dp),
+                                        horizontalArrangement = Arrangement.Center
+                                    ){
+                                        Column(modifier = Modifier.padding(20.dp)) {
+                                            Icon(
+                                                painter = painterResource(R.drawable.add_48px),
+                                                contentDescription = null,
+                                                modifier = Modifier.size(45.dp)
+                                            )
+                                        }
                                     }
                                 }
                             }
+                            item {
+                                Box(modifier = Modifier.padding(8.dp)){
+                                    ComponentCard(
+                                        component = "LTFRB",
+                                        date = "mm/dd/yyyy",
+                                        alarm = "3 months",
+                                        icon = painterResource(R.drawable.tire_repair)
+                                    )
+                                }
+                            }
+                            item {
+                                Box(modifier = Modifier.padding(8.dp)){
+                                    ComponentCard(
+                                        component = "Oil Change",
+                                        date = "mm/dd/yyyy",
+                                        alarm = "3 months",
+                                        icon = painterResource(R.drawable.tire_repair)
+                                    )
+                                }
+                            }
+                        }
+                    }
                 })
             }
         }
