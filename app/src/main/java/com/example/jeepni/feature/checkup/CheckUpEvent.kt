86a4684f9826cf4b89d863886d786b0420146131
miranda <emiranda@up.edu.ph>
2023-05-25 14:24:43 +0800

    added events for CheckUp

--- app/src/main/java/com/example/jeepni/feature/checkup/CheckUpEvent.kt
+++ app/src/main/java/com/example/jeepni/feature/checkup/CheckUpEvent.kt
@@ -1,17 +1,25 @@
 package com.example.jeepni.feature.checkup
 
-import com.example.jeepni.feature.analytics.AnalyticsEvent
+import androidx.compose.ui.geometry.Size
+import com.example.jeepni.core.data.model.AlarmContent
+import java.time.LocalDate
 
 sealed class CheckUpEvent {
     object OnBackPressed : CheckUpEvent()
-    data class OnAddComponentClicked(val value: Boolean): CheckUpEvent()
-    object OnTiresClicked: CheckUpEvent()
-    object OnSideMirrorsClicked: CheckUpEvent()
-    object OnWipersClicked: CheckUpEvent()
-    object OnEngineClicked: CheckUpEvent()
-    object OnSeatbeltClicked: CheckUpEvent()
-    object OnBatteryClicked: CheckUpEvent()
-    object OnOilChangeClicked: CheckUpEvent()
-    object OnLTFRBCheckClicked: CheckUpEvent()
-    object OnLTOCheckClicked: CheckUpEvent()
+    data class OnConfirmDelete ( val toDelete: MutableList<AlarmContent>): CheckUpEvent()
+
+    // shared by add and edit alarm
+    object OnDismiss: CheckUpEvent() // also used in delete alarm
+    object OnSaveClicked: CheckUpEvent()
+    data class OnNextAlarmChange ( val nextAlarm: LocalDate ): CheckUpEvent()
+    data class OnRepeatabilityChange ( val isRepeated: Boolean ): CheckUpEvent()
+    data class OnNumberChange ( val num: Int ): CheckUpEvent() // the enter number textfield (see Figma)
+    data class OnDurationChange ( val duration: String ): CheckUpEvent() // day? month? year?
+
+    // used by add alarm only
+    data class OnNameDropDownClick ( val isNameDropdownClicked:Boolean): CheckUpEvent()
+    data class OnNameSizeChange( val nameDropdownSize: Size): CheckUpEvent()
+    data class OnNameChange ( val name: Int): CheckUpEvent()
+
+
 }
