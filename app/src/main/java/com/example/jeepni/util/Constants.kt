8d077b8bd7e07e210606822d340f7b2b748445a0
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-05-25 14:56:10 +0800

    added constants for websocket events

--- app/src/main/java/com/example/jeepni/util/Constants.kt
+++ app/src/main/java/com/example/jeepni/util/Constants.kt
@@ -19,4 +19,8 @@ object Constants {
     const val NOTIFICATION_OBJECT = "notification_object"
 
     const val FIRST_NOTIFICATION_ID = 999
+
+    const val UPDATE_LOCATION = "update_location"
+    const val JOIN_ROOM = "join"
+    const val LEAVE_ROOM = "leave"
 }
