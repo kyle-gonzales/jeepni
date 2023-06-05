4aff7ee3efc1cc78f1db9b030b5d31ccda00a192
Kyle Gonzales <kyleangelog@gmail.com>
2023-06-05 23:35:53 +0800

    modified alarm content
    added copy method and added the intervalInDays property that is calculated based on the interval pair

--- app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
@@ -5,18 +5,33 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 class AlarmContent(
-    val name:String,
-    var isRepeatable: Boolean = false,
+    val name: String,
+    var isRepeatable: Boolean = true,
     var interval: Pair<Long, String> = Pair(3, "month"),
-    var nextAlarm:LocalDate = LocalDate.now().plusMonths(3)
+    var nextAlarmDate : LocalDateTime = LocalDate.now().plusMonths(3).atTime(7,0,0)
 ){
-    var nextAlarmDate: LocalDateTime = nextAlarm.atTime(23, 1, 1)
-    var intervalInDays : Int = 90 // repeat every 3 months
+    var intervalInDays = interval.first * when (interval.second) {
+        "day" -> 1
+        "month" -> 30
+        "year" -> 365
+        else -> 0
+    }
+
     init{
         when(interval.second){
             "day" -> nextAlarmDate = LocalDateTime.now().plusDays(interval.first)
             "month" -> nextAlarmDate = LocalDateTime.now().plusMonths(interval.first)
             "year" -> nextAlarmDate =  LocalDateTime.now().plusYears(interval.first)
         }
+    }
+
+
+    fun copy(newName : String = name, newIsRepeatable: Boolean = isRepeatable, newInterval: Pair<Long, String> = interval, newNextAlarmDate: LocalDateTime = nextAlarmDate ) :AlarmContent {
+        return AlarmContent(
+            newName,
+            newIsRepeatable,
+            newInterval,
+            newNextAlarmDate,
+        )
     }
 }
