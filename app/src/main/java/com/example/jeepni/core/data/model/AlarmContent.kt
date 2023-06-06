7886012aaf02bb327a0d2cb81ee00b6b593acc3f
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-06-07 01:37:31 +0800

    modified alarm content to be used as entity for database table
    intervalPair and nextAlarmDate are ignored because it is difficult to store non-primitive types in ROOM. Instead, those values are converted to strings, which are stored to the database
    
    an additional constructor is added. It is called when the data retrieved from the database is converted back to an AlarmContent object.
    
    added a main() to test the AlarmContent in a kotlin script

--- app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
+++ app/src/main/java/com/example/jeepni/core/data/model/AlarmContent.kt
@@ -1,21 +1,63 @@
 package com.example.jeepni.core.data.model
 
-import com.example.jeepni.util.Constants.ICON_MAP
-import java.time.LocalDate
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.example.jeepni.util.formatDateToString
+import com.example.jeepni.util.formatIntervalPairToString
+import com.example.jeepni.util.formatIntervalStringToPair
+import com.example.jeepni.util.formatStringToDate
 import java.time.LocalDateTime
 
+@Entity(tableName = "alarm")
 class AlarmContent(
-    val name: String,
+    var name: String = "",
     var isRepeatable: Boolean = true,
-    var interval: Pair<Long, String> = Pair(3, "month"),
-    var nextAlarmDate : LocalDateTime = LocalDate.now().plusMonths(3).atTime(7,0,0)
+
+    @Ignore var intervalPair: Pair<Long, String>,// = Pair(3, "month"),
+    @Ignore var nextAlarmDate: LocalDateTime,// = LocalDate.now().plusMonths(3).atTime(7,0,0)
 ){
-    var intervalInDays = interval.first * when (interval.second) {
+
+    constructor(name : String, isRepeatable: Boolean, interval: String, nextAlarm : String, ) : this(name, isRepeatable, formatIntervalStringToPair(interval), formatStringToDate(nextAlarm))
+
+    @PrimaryKey var id = nextAlarmDate.hashCode()
+    var intervalInDays = intervalPair.first * when (intervalPair.second) {
         "day" -> 1
         "month" -> 30
         "year" -> 365
         else -> 0
     }
+    var interval = formatIntervalPairToString(intervalPair)
+    var nextAlarm = formatDateToString(nextAlarmDate)
 
 
+}
+
+fun main() { // used for testing
+     val alarm = AlarmContent(
+         "Tires",
+         false,
+         "3 month",
+         "6-6-2023"
+     )
+    val a = alarm.nextAlarm
+    val i = alarm.interval
+    println(a)
+    println(i)
+    println("\n")
+
+    val b = formatStringToDate(alarm.nextAlarm)
+    val j = formatIntervalStringToPair(alarm.interval)
+    println((b == alarm.nextAlarmDate).toString())
+    println(b)
+    println(alarm.nextAlarmDate.toString()) // format string to date issue
+    println("\n")
+
+    val now = LocalDateTime.now().toLocalDate()
+    val new = now.minusDays(1).atTime(7,0,0)
+    println(now.toString())
+    println(LocalDateTime.now().minusDays(1).toLocalDate().atTime(7,0,0))
+    println(new.toString())
+    println(alarm.nextAlarmDate.hashCode() == new.hashCode())
+//    println(j.toString())
 }
