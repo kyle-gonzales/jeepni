3b452641fceb0770c8d83497a34848ae075811a4
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-06-09 02:18:10 +0800

    remove text under driving mode switch
    it looked ugly.

--- app/src/main/java/com/example/jeepni/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/jeepni/feature/home/MainScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -38,7 +37,6 @@ import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.jeepni.MainActivity
 import com.example.jeepni.R
-import com.example.jeepni.core.ui.JeepNiText
 import com.example.jeepni.core.ui.JeepNiTextField
 import com.example.jeepni.core.ui.PermissionDialog
 import com.example.jeepni.core.ui.theme.*
@@ -517,17 +515,16 @@ fun TopActionBar(
             actions = {
                 Column (
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(8.dp)
                 ) {
-                    Row(modifier = Modifier.padding(horizontal = 8.dp)) {
-                        Switch(
-                            checked = drivingMode,
-                            onCheckedChange = { toggleDrivingMode(it) },
-                            enabled = true,
-                            colors = SwitchDefaults.colors()
-                        )
-                    }
-                    JeepNiText(text = "Driver Mode", fontSize = 11.sp)
+                    Switch(
+                        checked = drivingMode,
+                        onCheckedChange = { toggleDrivingMode(it) },
+                        enabled = true,
+                        colors = SwitchDefaults.colors(),
+                        modifier = Modifier
+                    )
                 }
             }
         )
