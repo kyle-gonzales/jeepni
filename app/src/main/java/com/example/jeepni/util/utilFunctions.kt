5f27dbed37a826bf136fca28300ef41769137cb4
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-06-10 01:24:02 +0800

    add function to convert vectors to bitmap descriptors
    used for map pins

--- app/src/main/java/com/example/jeepni/util/utilFunctions.kt
+++ app/src/main/java/com/example/jeepni/util/utilFunctions.kt
@@ -5,11 +5,15 @@ import android.app.PendingIntent
 import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.example.jeepni.MainActivity
 import com.example.jeepni.R
 import com.example.jeepni.core.data.model.UserDetails
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 fun isIncompleteUserDetails(userDetails: UserDetails) : Boolean {
     return userDetails.route == "null" || userDetails.name == "null" || userDetails.name.isNullOrEmpty() || userDetails.route.isNullOrEmpty()
@@ -64,4 +68,24 @@ fun showSimpleNotification(
     with(NotificationManagerCompat.from(context)) {
         notify(notificationId, builder.build())
     }
+}
+
+fun bitmapDescriptorFromVector(
+    context: Context,
+    vectorResId: Int
+): BitmapDescriptor? {
+
+    // retrieve the actual drawable
+    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    val bm = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+
+    // draw it onto the bitmap
+    val canvas = android.graphics.Canvas(bm)
+    drawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bm)
 }
